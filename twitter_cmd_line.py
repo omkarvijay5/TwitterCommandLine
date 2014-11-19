48c31d0e800f7e259f15188609ac5e0c92bd69f5
omkarvijay5 <omkarvijay5@gmail.com>
2014-11-19 15:50:57 +0530

    modified init contstructor to check if access token exists if not enabled user to enter tokens through command line

--- twitter_cmd_line.py
+++ twitter_cmd_line.py
@@ -4,26 +4,40 @@ import sys
 
 
 class TwitterCmdLine(object):
-
+    
+    #Twitter Api initialization
     def __init__(self):
-
         consumer_key = os.environ.get('CONSUMER_KEY')
         consumer_secret = os.environ.get('CONSUMER_SECRET')
         access_token = os.environ.get('ACCESS_TOKEN')
         access_secret_token = os.environ.get('ACCESS_SECRET_TOKEN')
+        
+        #Enter the access tokens if no token exists
+        if not consumer_key or consumer_secret or \
+           not access_token or access_secret_token:
+            consumer_key = raw_input("please enter your twitter consumer key")
+            consumer_secret = raw_input("Please enter your twitter consumer secret")
+            access_token = raw_input("Please enter your twitter access token")
+            access_secret_token = raw_input("Please enter your twitter secret token")
         self.api = twitter.Api(consumer_key=consumer_key,
                                consumer_secret=consumer_secret,
                                access_token_key=access_token,
                                access_token_secret=access_secret_token)
 
+    #Displays all the friends list
     def show_friends(self):
-
         friends_list = self.api.GetFriends()
         for friend in friends_list:
             print friend.name
 
 if __name__ == '__main__':
     twitter_cmd = TwitterCmdLine()
-
-    if sys.argv[1] == 'friends':
-        twitter_cmd.show_friends()
+    try:
+        twitter_cmd.api.VerifyCredentials()
+    
+        #python twitter_cmd_line.py friends will display friends
+        if sys.argv[1] == 'friends':
+            twitter_cmd.show_friends()
+    
+    except twitter.TwitterError as errors:
+        print errors.message[0]['message']
